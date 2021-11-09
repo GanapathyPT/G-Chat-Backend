657c23672bfa828fbd0fa7b62db42ad8b5f41aba
GanapathyPT <18ganapathy04@gmail.com>
2021-11-09 18:04:12 +0530

    add: token views

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import UserRegisterView
 
-urlpatterns = [path("register/", UserRegisterView.as_view())]
+urlpatterns = [
+    path("register/", UserRegisterView.as_view()),
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+]
