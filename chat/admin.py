5a1ba43f46a08fc6de3ad17d029111821cc088f3
GanapathyPT <18ganapathy04@gmail.com>
2021-11-10 21:21:13 +0530

    add: chat models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room, Message, ReadStatus
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "created_by")
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "created_at")
+
+
+class ReadStatusAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "last_message", "room")
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(ReadStatus, ReadStatusAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Room(BaseModel):
+    # title is used for group chats
+    # for Personal Chat ther is no title
+    title = models.CharField(max_length=120, null=True, blank=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="room_owner"
+    )
+    users = models.ManyToManyField(User, related_name="room_users")
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+
+class Message(BaseModel):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} - {self.created_at}"
+
+
+class ReadStatus(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
